1a23b1d89c7a899f5d7936750b53812b068f0bc1
kismat <kismat@localhost.localdomain>
2018-08-08 01:38:08 +0100

    Update folder docs

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ import { view }   from './router_views'
 import  './styles/app.scss'
 
 
-const main = app(state, actions, view, document.body)
+const main = app(state, actions, view, document.querySelector('.hyperapp-root'))
 
 const unsubscribe = location.subscribe(main.location)
